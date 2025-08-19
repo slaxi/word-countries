186459e9dfb22ee8312ebb48730d80d44814b6da
Nenad Ivkovic <nenad_ivkovic@epam.com>
2025-08-19 12:31:36 +0200

    Adding snapshot for the App component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,23 +3,29 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('App componenent', () => {
-  it('should display correct title', () => {
+  beforeEach(() => {
     render(<App />);
+  });
+  it('should display correct title', () => {
     const heading = screen.getByRole('heading', { level: 1 });
 
     expect(heading).toBeInTheDocument();
   });
   it('should display correct title', () => {
-    render(<App />);
     const heading = screen.getByRole('heading', { level: 1 });
 
     expect(heading).toHaveTextContent('World Countries');
   });
 
   it('should display the list of continents', () => {
-    render(<App />);
     const headings = screen.getAllByRole('heading', { level: 2 });
 
     expect(headings).toHaveLength(5);
+  });
+
+  it('should create snapshot for the correct title', () => {
+    const { container } = render(<App />);
+
+    expect(container).toMatchSnapshot();
   });
 });
