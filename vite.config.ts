8db4c4eddb3d3cad2485c907041290bdc65ee302
Nenad Ivkovic <nenad_ivkovic@epam.com>
2025-08-19 12:24:57 +0200

    Added unit tests for App and HomeCart component

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./setupTests.js",
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
     include: ['src/__tests__/**/*.test.{js,jsx,ts,tsx}'],
     exclude: ['node_modules', 'dist']
   }
-})
+});
