db568da7cdd965b3edc2c8ae57c49490aa3fbc7f
Nenad Ivkovic <nenad_ivkovic@epam.com>
2025-08-25 14:06:12 +0200

    Error boundary and fallback components added. Fix prettier styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ErrorBoundary from './components/error/ErrorBoundary';
+import Fallback from './components/error/Fallback';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ErrorBoundary fallbackMessage={'Something went wrong!'}>
+        <App />
+      </ErrorBoundary>
     </QueryClientProvider>
   </StrictMode>
 );
