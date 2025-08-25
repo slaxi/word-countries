2f2645e03b05e773058dfaa395f0b06727aabc62
Nenad Ivkovic <nenad_ivkovic@epam.com>
2025-08-25 13:36:33 +0200

    Adjust type and naming convention

--- src/utils/dataFetch.ts
+++ src/utils/dataFetch.ts
@@ -7,7 +7,7 @@ export type TErrorResponse = {
 
 export const dataFetch = async (query: string | null): Promise<TCountryList[] | TErrorResponse> => {
   try {
-    const response = await fetch(`${BASE_COUNTRIES_API}${query}`);
+    const response = await fetch(`${BASE_COUNTRIES_API}${query ?? ''}`);
     if (!response || !response.ok) throw Error('Something went wrong! No data fetch!');
     const data = await response.json();
     return data as TCountryList[];
