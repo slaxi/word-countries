971484b92d801f2c90d88067c40a88f24e7de0cb
Nenad Ivkovic <nenad_ivkovic@epam.com>
2025-08-25 15:17:40 +0200

    Add test for error boundary

---
+++ src/__tests__/HomePage.error.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+import HomePage from '../components/home/HomePage';
+
+vi.mock('../hooks/useFetchData', () => ({
+  useFetchData: () => ({
+    data: null,
+    isLoading: false,
+    error: 'No data found!'
+  })
+}));
+
+describe('Fallback message', () => {
+  it.only('should display fallback message if there is a error in data fetch', () => {
+    render(<HomePage />);
+
+     expect(screen.getByText('No data found!')).toBeInTheDocument();
+  });
+});
